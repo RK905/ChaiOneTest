e3fad0cd84b08b784e4b447b5a730ca1504285f5
RK905 <mr.rayenk@gmail.com>
2015-10-20 17:56:33 -0400

    Initial Commit

---
+++ ChaiOneTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChaiOneTest
+//
+//  Created by Rayen Kamta on 10/20/15.
+//  Copyright © 2015 Geeksdobyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChaiOneTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChaiOneTest
+//
+//  Created by Rayen Kamta on 10/20/15.
+//  Copyright © 2015 Geeksdobyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
